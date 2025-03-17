967dae2c04ff4702cc2d81354eb5974efde9d3b4
Nabeel Nizam <abee237@gmail.com>
2025-03-17 23:55:37 +0700

    CRUD dengan ajax

---
+++ resources/views/user/create_ajax.blade.php
@@ -0,0 +1,90 @@
+<form action="{{ url('/user/ajax') }}" method="POST" id="form-tambah">
+    @csrf
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data User</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label>Level Pengguna</label>
+                    <select name="level_id" id="level_id" class="form-control" required>
+                        <option value="">- Pilih Level -</option>
+                        @foreach ($level as $l)
+                            <option value="{{ $l->level_id }}">{{ $l->level_nama }}</option>
+                            @endforeach
+                    </select>
+                    <small id="error-level_id" class="error-text form-text text-danger"></small>
+                </div>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input value="" type="text" name="username" id="username" class="form-control" required>
+                    <small id="error-username" class="error-text form-text text-danger"></small>
+                </div>
+                <div class="form-group">
+                    <label>Nama</label>
+                    <input value="" type="text" name="nama" id="nama" class="form-control" required>
+                    <small id="error-nama" class="error-text form-text text-danger"></small>
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input value="" type="password" name="password" id="password" class="form-control" required>
+                    <small id="error-password" class="error-text form-text text-danger"></small>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+        </div>
+    </div>
+</form>
+<script>
+    $(document).ready(function() {
+        $("#form-tambah").on('submit', function(e) {
+            e.preventDefault(); // Mencegah submit biasa
+
+            $.ajax({
+                url: $(this).attr('action'),
+                type: $(this).attr('method'),
+                data: $(this).serialize(),
+                dataType: 'json',
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest' // Paksa agar dianggap AJAX
+                },
+                success: function(response) {
+                    if (response.status) {
+                        $('#myModal').modal('hide');
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Berhasil',
+                            text: response.message
+                        });
+                        dataUser.ajax.reload();
+                    } else {
+                        $('.error-text').text('');
+                        $.each(response.msgField, function(prefix, val) {
+                            $('#error-' + prefix).text(val[0]);
+                        });
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Terjadi Kesalahan',
+                            text: response.message
+                        });
+                    }
+                },
+                error: function(xhr) {
+                    console.error("Error:", xhr.responseText);
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Gagal',
+                        text: 'Terjadi kesalahan saat mengirim data'
+                    });
+                }
+            });
+        });
+    });
+</script>
